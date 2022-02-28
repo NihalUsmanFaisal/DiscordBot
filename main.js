f1bd4dfaca1fe13e96da6950c8cc483b2dc27232
NihalUsmanFaisal <nihalusman@gmail.com>
2022-02-28 18:20:23 +0530

    Updated command look up structure

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const {Client,Intents} = require("discord.js");
+const {Client,Intents, Collection} = require("discord.js");
 const {token} = require("./config.json");
+const fs = require("fs");
 
 const client  = new Client({ intents:[Intents.FLAGS.GUILDS]});
+client.commands = new Collection();
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith('.js'));
+for(index in commandFiles){
+	var command = require(`./commands/${commandFiles[index]}`);
+	client.commands.set(command.data.name,command);
+}
 
 client.once('ready',()=>{
     console.log("Ready!");
 })
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+	client.commands.get(commandName).execute(interaction);
+
+});
 
 client.login(token);
 
